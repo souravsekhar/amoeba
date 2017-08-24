b8919254bb7e0b79d8b7ea96e500ed2845f2de24
Sahoo <sekhar.sourav8@gmail.com>
2017-08-24 09:04:17 -0400

    added file upload logic

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,44 @@
 
 import UploadHandler from '../server/handlers/uploadHandler.js';
 
-const routes = [  
-{
-	method: 'POST',
-	path: '/upload',
-	handler: function(request, reply) {        
-        reply.view('upload', {data});
-    }
-},
-{
-    method: 'GET',
-    path: '/show',
-    handler: function(request, reply) {
-        // Render the view with the custom greeting    
-        reply.view('show', {});
-    }
-},
-{  
-	method: 'GET',
-	path: '/css/{file*}',
-	handler: {
-        directory: {
-            path: 'public/lib/css',
-            listing: true
+const routes = [
+    {// homepage of the app
+    	method: 'GET',
+    	path: '/upload',
+    	handler: function (request, reply) {
+            reply.view('upload', {});
         }
-	}
-}
+    },
+    {// show processed images
+        method: 'GET',
+        path: '/show',
+        handler: function(request, reply) {
+            reply.view('show', {});
+        }
+    },
+    {// upload images
+        method: 'POST',
+        path: '/image/upload',
+        config: {
+            payload: {
+                maxBytes: 209715200,
+                output: 'stream',
+                parse: false,
+                allow: 'multipart/form-data'
+            }
+        },
+        handler: UploadHandler.uploadHandler
+    },
+    {// serve css files
+    	method: 'GET',
+    	path: '/css/{file*}',
+    	handler: {
+            directory: {
+                path: 'public/lib/css',
+                listing: true
+            }
+    	}
+    }
 ];
 
 module.exports = routes;
--- server/handlers/uploadHandler.js
+++ server/handlers/uploadHandler.js
@@ -1,1 +1,24 @@
 'use strict';
+
+import fs from 'fs';
+import multiparty from 'multiparty';
+
+const uploadHandler = (req, reply) => {// uploads the received image to disk currently
+	let form = new multiparty.Form();
+	form.parse(req.payload, (error, fields, files) => {
+		if (error) return error;
+		fs.readFile(files.imageFile[0].path, (error, data) => {
+			if (error) return error;
+			let newPath = "./uploads/" + files.imageFile[0].originalFilename; //TODO: create unique file names
+			fs.writeFile(newPath, data, (error) => {
+				if (error) return error;
+			});
+		});
+	});
+
+	reply("Uploaded");
+}
+
+module.exports = {
+	uploadHandler: uploadHandler
+};

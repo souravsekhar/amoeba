0391480b48cf56d9692e649f5d596771a35c1207
Sahoo <sekhar.sourav8@gmail.com>
2017-08-25 07:30:07 -0400

    added image type validation

--- server/handlers/uploadHandler.js
+++ server/handlers/uploadHandler.js
@@ -3,22 +3,27 @@
 import fs from 'fs';
 import multiparty from 'multiparty';
 import ImageProcessor from './imageProcessor'
+import path from 'path';
 
 const uploadHandler = (req, reply) => {// uploads the received image to disk currently
-	let form = new multiparty.Form();
+	let form = new multiparty.Form(),
+		allowedImageTypes = ['.png', '.jpg'];
 	form.parse(req.payload, (error, fields, files) => {
 		if (error) return error;		
 		fs.readFile(files.imageFile[0].path, (error, data) => {
 			if (error) return error;
-			let newPath = "./uploads/originals/" + files.imageFile[0].originalFilename; //TODO: create unique file names
-			fs.writeFile(newPath, data, (error) => {
-				if (error) return error;
-				ImageProcessor.imageProcessor(newPath);
-			});
+			if (allowedImageTypes.indexOf(path.extname(files.imageFile[0].path)) > -1){// validates the file extension
+				let newPath = "./uploads/originals/" + files.imageFile[0].originalFilename; //TODO: create unique file names
+				fs.writeFile(newPath, data, (error) => {// creates a new file if doesn't exist and overwrites the existing one if exists
+					if (error) return error;
+					ImageProcessor.imageProcessor(newPath);
+					reply('Uploaded');
+				});
+			}else {
+				reply('You cannot upload' + path.extname(files.imageFile[0].path) + ' files');
+			}
 		});
 	});
-
-	reply("Uploaded");
 }
 
 module.exports = {

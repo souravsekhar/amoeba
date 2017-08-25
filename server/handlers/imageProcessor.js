6b2f28e735522aecb0a04142e780fc0a6f95ffc4
Sahoo <sekhar.sourav8@gmail.com>
2017-08-25 06:30:13 -0400

    implemented image resizing feature

--- server/handlers/imageProcessor.js
+++ server/handlers/imageProcessor.js
@@ -3,57 +3,85 @@
 import Jimp from 'jimp';
 import fs from 'fs';
 
-// const imageConfig = {
-//   large: {
-//     dimensions:,
-//     formats: {[
-//         {
-//           type: png,
-//           quality:,
-//         },
-//         {
-//           type: jpg,
-//           quality:,
-//         }
-//     ]}
-//   },
-//   medium: {
-//     dimensions:,
-//     formats: {[
-//           {
-//             type: png,
-//             quality:,
-//           },
-//           {
-//             type: jpg,
-//             quality:,
-//           }
-//       ]}
-//   }
-//   small: {
-//     dimensions:,
-//     formats: {[
-//         {
-//           type: png,
-//           quality:,
-//         },
-//         {
-//           type: jpg,
-//           quality:,
-//         }
-//     ]}
-//   }
-// };
+// predefined image configuration
+const imageConfig = {
+  "large": {
+    "dimensions": {
+      "height": 1000,
+      "width": 1000
+    },
+    "formats": [
+        {
+          "type": "png",
+          "quality": 99
+        },
+        {
+          "type": "jpg",
+          "quality": 60
+        }
+    ]
+  },
+  "medium": {
+    "dimensions": {
+      "height": 700,
+      "width": 700
+    },
+    "formats": [
+          {
+            "type": "png",
+            "quality": 99
+          },
+          {
+            "type": "jpg",
+            "quality": 60
+          }
+      ]
+  },
+  "small": {
+    "dimensions": {
+      "height": 200,
+      "width": 200
+    },
+    "formats": [
+        {
+          "type": "png",
+          "quality": 99
+        },
+        {
+          "type": "jpg",
+          "quality": 60
+        }
+    ]
+  }
+}
 
 const imageProcessor = (imagePath) => {
-  Jimp.read(imagePath, function (err, image) {
-      if (err) throw err;            
-      image.resize(256, 256)            // resize
-           .quality(60)                // set greyscale
-           .write("./processedImages/processed_image.png"); // save
-  });
+  Jimp.read(imagePath, function (err, image) {// reading the uploaded file
+    if (err) return err;
 
-};
+    for (let size in imageConfig) {      
+
+        let height = imageConfig[size].dimensions.height;
+        let width = imageConfig[size].dimensions.width;
+
+        imageConfig[size].formats.forEach((format) => {          
+          switch (format.type) {
+            case 'png':            
+              image.resize(height, width)
+                   .rgba(true) 
+                   .filterType( Jimp.PNG_FILTER_AUTO )
+                   .write(`./uploads/${size}/PNG/processed_image_png_${size}.png`);             
+              break;
+            case 'jpg':           
+              image.resize(height, width)
+                   .quality(format.quality)
+                   .write(`./uploads/${size}/JPG/processed_image_jpg_${size}.jpg`);             
+              break;            
+          }
+      });                
+    }
+  });        
+}
 
 module.exports = {
   imageProcessor: imageProcessor
--- server/handlers/uploadHandler.js
+++ server/handlers/uploadHandler.js
@@ -7,11 +7,10 @@ import ImageProcessor from './imageProcessor'
 const uploadHandler = (req, reply) => {// uploads the received image to disk currently
 	let form = new multiparty.Form();
 	form.parse(req.payload, (error, fields, files) => {
-		if (error) return error;
-		console.log("files == ", files);
+		if (error) return error;		
 		fs.readFile(files.imageFile[0].path, (error, data) => {
 			if (error) return error;
-			let newPath = "./uploads/" + files.imageFile[0].originalFilename; //TODO: create unique file names
+			let newPath = "./uploads/originals/" + files.imageFile[0].originalFilename; //TODO: create unique file names
 			fs.writeFile(newPath, data, (error) => {
 				if (error) return error;
 				ImageProcessor.imageProcessor(newPath);
